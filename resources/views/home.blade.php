d1f709ef1346de67e703c292109e1826e50102f7
InaamHoj <inaamhojeij@live.com>
2022-04-29 11:47:42 +0200

    home commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,19 +5,43 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('Get Started!') }}</div>
+    
 
                 <div class="card-body">
+
+                
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                        <div class="alert alert-success" role="alert">  
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    <a href="{{ route ('lists.create') }}"> <input type="button" value="Créer vos catégories"></a>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+
+    <div class="card">
+        <h1>Categories</h1>
+        <div class="categorieslists">
+        @foreach ($lists as $list)
+
+            <div> {{ $list->category }} </div>
+        @endforeach
+</div>
+
+    </div>
+
+  
+@if(session()->has('message'))
+            <div class="alert alert-success">
+                {{ session()->get('message') }}
+            </div>
+        @endif
+</div>
 @endsection
+
+
