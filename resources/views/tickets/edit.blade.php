ba33bdfe9b426f582e9f6108b813d00fa6587039
StephDM06 <dumasmiton.stephane@ecomail.fr>
2022-05-03 15:37:22 +0200

    Commit AM 0305

--- resources/views/tickets/edit.blade.php
+++ resources/views/tickets/edit.blade.php
@@ -4,13 +4,13 @@
 
 <div class="row justify-content-center">
     <div class="col-4 todo">
-        <form action="{{ route('tickets.update', $ticket->id)}}" method="POST">
+        <form action="{{ route('tickets.update', $ticket)}}" method="POST">
         @csrf
         @method('PUT')
 
         <label for="category">Modifier votre ticket</label>
         <br>
-        <input type="text" name="category" value="{{$ticket->content}}">
+        <input type="text" name="content" value="{{$ticket->content}}">
         <button type="submit" class="btn btn-success">Mettre à jour</button>
         </form>
     </div>
